d3cfa468d5bad129a91df0e19aa5e6f463e0517d
Louis Chatain <louischatain123@gmail.com>
2025-10-02 01:02:08 +0200

    connected the API with postgresql

--- back-end/config.py
+++ back-end/config.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 import os
 
 
 backend_path = os.path.dirname(os.path.abspath(__file__))
-app = Flask(__name__, instance_path=os.path.join(backend_path, "../databse/instance"))
+app = Flask(__name__) #, instance_path=os.path.join(backend_path, "../databse/instance")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:patate@localhost:5433/expense"
+app.secret_key = "whatinthehellamisupposedtowritehere?generateanultrastrongpassword?"
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:patate@localhost:5433/test"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
